d166da34611c0a9fa46755c7c2db444c9f66a8a2
MrigElcoder <88018113+MrigElcoder@users.noreply.github.com>
2021-08-20 20:36:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,16 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-
+     let time=new Date().toLocaleTimeString();
+        const [ctime,setctime]=useState(time);
+        const UpdateTime=()=>{
+            time=new Date().toLocaleTimeString();
+            setctime(time);
+        };
+        setInterval(UpdateTime,1000);
         return(
             <>
-               
+               <h1>{ctime}</h1>
             </>
         )
     }
